093962a9c7ee1c64c876a7d8f9b1e2d38331dac1
Maxi Shea <shea.maximiliano@hotmail.com>
2023-04-25 20:37:42 -0300

    first commit

---
+++ src/components/NavBar/Navbar.js
@@ -0,0 +1,13 @@
+import * as S from './Navbar.styles'
+
+const Navbar = () => {
+  return (
+
+    <S.ContainerNavbar>
+        Nuestro container navbar
+    </S.ContainerNavbar>
+
+  )
+}
+
+export default Navbar
